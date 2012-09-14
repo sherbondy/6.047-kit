14660ce3d4b8beb76eb9ae4558c8c4c1745aa1ac
Ethan Sherbondy <sherbondye@gmail.com>
2012-09-14 19:24:27 -0400

    pretty-print matrix

--- src/pset1/core.clj
+++ src/pset1/core.clj
@@ -1,9 +1,52 @@
-(ns pset1.core)
+(ns pset1.core
+  (:require [incanter.core :as i]))
 
-(def nuc-scores (merge (same-nuc-scores) (pp-nuc-scores)))
+(defn same-nuc-scores []
+  (into {}
+    (for [nuc [:A :T :C :G]]
+      {nuc 1})))
+
+(def vreverse (comp vec reverse))
 
 (defn pp-nuc-scores []
-  (into {} (for [pair [[:A :G] [:T :C]]]
-             {pair -1/2 (vreverse pair) -1/2})))
+  (into {}
+    (for [pair [[:A :G] [:T :C]]]
+      {pair -1/2 (vreverse pair) -1/2})))
 
-(nuc-scores [:A :T] -1) ;; default to -1 for undefined scores
+(def nuc-scores 
+  (merge (same-nuc-scores) (pp-nuc-scores)))
+
+(def neg-inf Integer/MIN_VALUE)
+
+(defn s-matrix [v w]
+  (let [v-len (count v) 
+        w-len (count w)]
+    (apply vector 
+      (for [i (range v-len)]
+        (int-array
+          (for [j (range w-len)]
+            (if (or (= j 0) (= i 0))
+              0
+              neg-inf)))))))
+
+(defn ppm [m]
+  (print "[\n")
+  (doseq [row m]
+    (print (vec row) "\n"))
+  (print "]\n"))
+
+
+;; use get-in for easy access
+
+(defn lcs 
+  "Finds the longest common subsequence of two seqs v and w"
+  [v w]
+  (let [v-len (count v) w-len (count w)
+        s (apply vector 
+            (for [i (range v-len)]
+              (int-array
+                (for [j (range w-len)]
+                  (if (or (= j 0) (= i 0))
+                    0
+                    neg-inf)))))]
+    s))
