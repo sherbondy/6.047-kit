2a2344d20af6eee558893c12d82a6b45704b870c
Ethan Sherbondy <sherbondye@gmail.com>
2012-09-14 23:12:21 -0400

    working lcs algo, based on https://github.com/hoeck/seq-diff/blob/master/src/hoeck/diff/lcs.clj

--- src/pset1/core.clj
+++ src/pset1/core.clj
@@ -16,32 +16,64 @@
 (def nuc-scores 
   (merge (same-nuc-scores) (pp-nuc-scores)))
 
-(def neg-inf Integer/MIN_VALUE)
 
-(defn s-matrix [n m]
-  (vec
-    (for [i (range (inc n))]
-      (vec
-        (for [j (range (inc m))]
-          (if (or (= j 0) (= i 0))
-            0
-            neg-inf))))))
-
-(defn b-matrix [n m]
-  (vec (repeat (inc n) (vec (repeat (inc m) nil)))))
 
 (defn ppm [m]
   (print "[\n")
   (doseq [row m]
-    (print row "\n"))
+    (print (vec row) "\n"))
   (print "]\n"))
 
+(defn seti [a [i j] v]
+  (aset a (int i) (int j) (int v)))
 
-;; use get-in for easy access
+(defn getin [a [i j]]
+  (aget a i j))
 
-(defn lcs 
-  "Finds the longest common subsequence of two seqs v and w"
-  [v w]
-  (let [n (count v) m (count w)
-        s (s-matrix n m)]
+(defn s-matrix [rows cols]
+  (let [s (make-array Integer/TYPE rows cols)]   
+    (doseq [r (range rows)
+            c (range cols)]
+      (seti s [r 0] 0)
+      (seti s [0 c] 0))
     s))
+
+(defn b-matrix [rows cols]
+  (let [b (make-array clojure.lang.Keyword rows cols)]))
+
+(defn lcs-matrix
+  "Builds and fills a matrix to find the longest common subsequence
+  (LCS) of two sequences s and t.
+  Both must be Indexed (supporting the nth method).
+  Returns a twodimensional array of Integers"
+  [v w]
+  (let [rows (inc (count v))
+        cols (inc (count w))
+        s (s-matrix rows cols)]
+    (doseq [i (range 1 rows)]
+      (doseq [j (range 1 cols)]
+        (seti s [i j]
+          (if (= (v (dec i)) (w (dec j)))
+            (inc (getin s [(dec i) (dec j)]))
+            (apply max (map (partial getin s) 
+                          [[i (dec j)]
+                          [(dec i) j]]))))))
+    s))
+
+(defn lcs-vec
+  "Given a lcs-matrix (created with the lcs-matrix function) and the
+  corresponding sequences, returns the actual lcs as a vector."
+  [lcs-matrix s t & {:keys [compare] :or {compare =}}]
+  (let [m lcs-matrix
+        f (fn lcs-vec-rec [i j]
+            (cond (or (= 0 i) (= 0 j))
+                  []
+                  (compare (nth s (dec i)) (nth t (dec j)))
+                  (conj (lcs-vec-rec (dec i) (dec j)) (nth s (dec i)))
+                  (< (aget m i (dec j)) (aget m (dec i) j))
+                  (lcs-vec-rec (dec i) j)
+                  :else
+                  (lcs-vec-rec i (dec j))))]
+    (f (count s) (count t))))
+
+
